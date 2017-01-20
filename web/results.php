cf1b340e865f48861a3cc196e362bc4122d7ccf5
Zac Cianflone <zcianflone@gmail.com>
2017-01-20 00:50:15 -0500

    testing php files progress bars 7

--- web/results.php
+++ web/results.php
@@ -74,6 +74,8 @@
     	echo "<h2>Survey Results<h2><hr>";
     	echo "<h3><small>Total Number of Votes: </small>". $resultsAssoc["TotVotes"];
     	
+    	//$progressColor = "green";
+    	
     	foreach ($resultsAssoc as $x => $x_value){
     	
     	//category name will have value of -1.  when we hit that value, we change the progress bar color and output the header
@@ -82,19 +84,22 @@
     	echo "<h3><small>" . $x . "</small></h3><hr>";
     	}
     	
-    	else if ($x != "TotVotes"){
     	echo "<div class=\"col-sm-6\">"
+    	
+    	else if ($x != "TotVotes"){
+    	
     	echo "<h4><small>" . $x . "</small></h4>";
     	echo "<div class=\"progress\">";
     	echo "<div class=\"progress-bar progress-bar-success progress-bar-striped active\" role=\"progressbar\"";
     	echo "aria-valuenow=\"".$x_value."\" aria-valuemin=\"0\" aria-valuemax=\"".$resultsAssoc["TotVotes"]."\" style=\"width:".($x_value/$resultsAssoc["TotVotes"])*100 ."%\">";
     	echo $x_value;
-    	echo "</div>";
+    	
 		echo "</div>";
 		echo "</div>";
 		echo "<br> <br>";
 		}
 		
+		echo "</div>";
 		}
     	
     	
