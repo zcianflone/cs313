9bec902b1c040e15262eaf63a3861d1457671f39
Zac Cianflone <zcianflone@gmail.com>
2017-01-19 23:24:24 -0500

    testing php files 14

--- web/results.php
+++ web/results.php
@@ -61,7 +61,7 @@
     	$resultsFile = "results.txt";
     	$fh = fopen($resultsFile, 'r'); //opening file
     	$theData = fread ($fh, filesize($resultsFile)); //makes a big string out of file
-    	$resultsAssoc = array ();
+    	/*$resultsAssoc = array ();
     	$my_array = explode ("\n", $resultsAssoc); //explodes string into array.
     	foreach ($my_array as line)
     	{
