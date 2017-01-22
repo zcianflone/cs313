03d4aca488286ff2ab750cfd6c34a2705bcdfe37
Zac Cianflone <zcianflone@gmail.com>
2017-01-22 03:29:53 -0500

    testing staticphp

--- web/staticresults.php
+++ web/staticresults.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Computing Survey</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
-</head>
-<body>
 
-    
-    
-    <div>
     	<?php  
     	$resultsFile = "results.txt";
     	$fh = fopen($resultsFile, 'r'); //opening file
@@ -55,10 +40,6 @@
 
     	
     	?>
-    </div>
 
-
-</body>
-</html>
 
  
