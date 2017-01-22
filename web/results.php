6d2f93481ebaa3c5b82c01986dffaa90181529cb
Zac Cianflone <zcianflone@gmail.com>
2017-01-22 00:57:35 -0500

    testing php files progress bars 7

--- web/results.php
+++ web/results.php
@@ -71,6 +71,7 @@
     		
     		if ($tmp[0] == $_POST['OS']){
     			var_dump($resultsAssoc);
+    			(int)($tmp[1]++);
     			$resultsAssoc[$tmp[0]] = (int)($tmp[1]++);
     			var_dump($resultsAssoc);
     			echo "Over here!". $resultsAssoc[$tmp[0]] = $tmp[1];
