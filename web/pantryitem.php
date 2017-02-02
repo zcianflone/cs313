4b816b88d34d62679aafed53076ac6b836f80dd0
Zac Cianflone <zcianflone@gmail.com>
2017-02-02 00:14:30 -0500

    adding list view

--- web/pantryitem.php
+++ web/pantryitem.php
@@ -1,8 +1,25 @@
 <html>
 <body>
 
-<?php 
-echo "Hello " . $_GET['itemName'];
+
+
+<?php
+
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$itemName = str_replace('_',' ', $_GET['itemName']);
+
+echo "Hello " . $itemName;
 ?>
 
 </body>
