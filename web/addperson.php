142e5df06e3c1e3973ae668269fd1527b39bf32d
Zac Cianflone <zcianflone@gmail.com>
2017-02-09 01:54:43 -0500

    add person

---
+++ web/addperson.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+
+
+<?php
+
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$sql = "INSERT INTO person (kait, test);"
+
+$db->query($sql);
+?>
+
+</body>
+</html>
