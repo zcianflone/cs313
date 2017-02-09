ff148460b61b2a99edc770c42ba74524e694b4b1
Zac Cianflone <zcianflone@gmail.com>
2017-02-09 16:31:35 -0500

    testing ajax

--- web/addperson.php
+++ web/addperson.php
@@ -1,28 +1,29 @@
-<html>
-<body>
-
-
-
 <?php
 
-$dbUrl = getenv('DATABASE_URL');
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+        $password = stip_tags(trim($_POST["password"]));
 
-$dbopts = parse_url($dbUrl);
+        if ( empty($name) OR empty($password)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
+        else
+        {
+           // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        }
 
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
 
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
 
-$sql = "INSERT INTO person(name, password) VALUES ('kait', 'test')";
-
-$db->query($sql);
-
-echo "done!";
 ?>
-
-</body>
-</html>
