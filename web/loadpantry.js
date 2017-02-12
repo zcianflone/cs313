f9047b6174b437368313c0a445444264cdb81d52
Zac Cianflone <zcianflone@gmail.com>
2017-02-12 15:47:31 -0500

    added dbconnect

---
+++ web/loadpantry.js
@@ -0,0 +1,11 @@
+$(function() {
+
+	var listButton = $('#pantryButton');
+
+    // Get the list.
+    var list = $('#pantryList');
+
+	$(listButton).click(function(){
+        $(this).hide();
+    });
+});
