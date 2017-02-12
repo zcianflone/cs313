2c7c18398c4ab390713f3a8663757a4000efeb03
Zac Cianflone <zcianflone@gmail.com>
2017-02-12 17:56:20 -0500

    added dbconnect

--- web/loadpantry.js
+++ web/loadpantry.js
@@ -7,7 +7,7 @@ $(function() {
 
 	$(listButton).click(function(){
 	
-	$(list).text("<li class=\"list-group-item\">First item</li>");
+	$(list).text("<ul class=\"list-group\"><li class=\"list-group-item\">First item</li></ul>");
 
     });
 });
