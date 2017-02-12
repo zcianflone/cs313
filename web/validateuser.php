3c69ad0c44cf69a7dbef2b56815ed0c395d80441
Zac Cianflone <zcianflone@gmail.com>
2017-02-12 00:31:39 -0500

    added dbconnect

---
+++ web/validateuser.php
@@ -0,0 +1,38 @@
+<?php
+
+	require ("dbConnect.php");
+	$db = get_db();
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+        $password = strip_tags(trim($_POST["password"]));
+
+        if ( empty($name) OR empty($password)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
+        else
+        {
+           // Set a 200 (okay) response code.
+            http_response_code(200);
+            
+            $sql = "INSERT INTO person(name, password) VALUES ('" . $name . "','" . $password . "')";
+			$db->query($sql);
+
+            echo "New User \"" . $name . "\" has been added.";
+        }
+
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+    
+    //TO DO Add delete user functionality
+
+?>
