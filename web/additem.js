cae32d580a9d6e153b386bc4ae5ae1fef3229821
Zac Cianflone <zcianflone@gmail.com>
2017-02-12 22:54:28 -0500

    added dbconnect

--- web/additem.js
+++ web/additem.js
@@ -22,7 +22,7 @@ var formData = $(form).serialize();
 $.ajax({
     type: 'POST',
     url: $(form).attr('action'),
-    data: {formData : 'formData', id : 'id'}
+    data: formData+'&id='+id
 })
 .done(function(response) {
     // Make sure that the formMessages div has the 'success' class.
