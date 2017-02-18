6d9f53562d6f185c10b387c924fbbf3ebd585b39
Zac Cianflone <zcianflone@gmail.com>
2017-02-18 18:12:23 -0500

    test

--- web/loadpantry.js
+++ web/loadpantry.js
@@ -1,18 +1,31 @@
 $(function() {
 
 	var listButton = $('#pantryButton');
+	
+	
+	loadPantry();
 
     // Get the list.
     var list = $('#accordion');
 
 	$(listButton).click(function(){
-	
-			$(list).empty();
+			
+			
+			loadPantry();
 	
 
 	
 			// Submit the form using AJAX.
-		$.ajax({
+		
+
+    });
+});
+
+
+function loadPantry(){
+
+	$(list).empty();
+	$.ajax({
 			type: 'POST',
 			url: "loadpantry.php",
 			//data: formData
@@ -101,21 +114,5 @@ $(function() {
 		});
 
 
-    });
-});
+}
 
-
-/*<div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-        Collapsible Group 1</a>
-      </h4>
-    </div>
-    <div id="collapse1" class="panel-collapse collapse in">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
-    </div>
-  </div> */
