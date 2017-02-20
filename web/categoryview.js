16abc7639e2eb519b55b9f95db1b4be408941908
Zac Cianflone <zcianflone@gmail.com>
2017-02-20 04:45:42 -0500

    adding delete functionality

--- web/categoryview.js
+++ web/categoryview.js
@@ -6,7 +6,29 @@ $(function() {
 
 	$(select).change(function(){
 	
-	console.log("working!");
+		console.log(select.val());
+	
+		/*$.ajax({
+			type: 'POST',
+			url: "categoryview.php", 
+			data: 
+		})
+		.done(function(response) {
+		
+		
+		
+			var json = JSON.parse(response);
+			
+			for (i in json){
+			
+				var categoryName = json[i].name;
+				
+				/*var toAdd = "<option value=" + json[i].id + ">" + categoryName + "</option>";
+				
+				$(select).append(toAdd);
+				
+
+			}*/
 	
 	});
 
---
+++ web/categoryview.php
@@ -0,0 +1,34 @@
+<?php
+
+	session_start();
+
+	require ("dbConnect.php");
+	$db = get_db();
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    			$idquery = $db->prepare("SELECT id FROM person WHERE name = :name");
+    		    $idquery -> bindParam(':name', $_SESSION['username']);
+    		    $idquery -> execute();
+    		    $idresult =  $idquery -> fetch(PDO::FETCH_ASSOC);
+				$id = $idresult['id'];
+    	
+     			$stmt = $db->prepare("SELECT * FROM item INNER JOIN itemcategory ON item.id = itemcategory.item_id WHERE itemcategory.category_id='10' AND itemcategory.person_id = '25'");
+     			$stmt -> bindParam(':id', $id);
+				$stmt -> execute();
+				$result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+				$jsonresult = json_encode($result);
+				
+				echo $jsonresult;
+    
+    
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+    
+    //TO DO Add delete user functionality
+
+?>
