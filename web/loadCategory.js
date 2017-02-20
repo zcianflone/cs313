9376c4c43ac0d5f0b8f09344ca665dcfda5b7e57
Zac Cianflone <zcianflone@gmail.com>
2017-02-19 21:32:49 -0500

    adding delete functionality

--- web/loadCategory.js
+++ web/loadCategory.js
@@ -1,1 +1,67 @@
-loadCategory()
+function loadCategory(){
+
+  // Get the selects
+    var select = $('.categorySelect');
+
+	$(select).empty();
+	
+	$.ajax({
+			type: 'POST',
+			url: "loadCategory.php"
+		})
+		.done(function(response) {
+		
+		
+		
+			var json = JSON.parse(response);
+			
+			for (i in json){
+			
+				var categoryName = json[i].name;
+				
+				var toAdd = "<option value=" + json[i].id + ">" + categoryName + "</option>";
+				
+				$(select).append(toAdd);
+				
+
+			}
+			
+			
+		})
+		.fail(function(data) {
+	
+		});
+
+
+}
+
+
+
+$(function() {
+
+	
+	//on page load, we empty and fill all category selects
+  	var select = $('.categorySelect');
+
+	$(select).empty();
+	
+	loadCategory();
+	
+	var catButton = $('#addCatButton');
+
+
+	$(catButton).click(function(){
+			
+			$(select).empty();
+			loadCategory();
+			
+
+    });
+    
+    
+
+  
+    
+    
+  
+});
