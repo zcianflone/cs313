2c68d0587c4cfdb521be7c7a88efa932e2ee5f96
Zac Cianflone <zcianflone@gmail.com>
2017-02-20 04:35:31 -0500

    adding delete functionality

---
+++ web/categoryview.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	
+	//on page load, we empty and fill all category selects
+  	var select = $('#categoryspinner');
+
+	$(select).change(function(){
+	
+	console.log("working!");
+	
+	}
+
+  
+});
